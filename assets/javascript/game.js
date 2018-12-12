1383cb033fe011adb7b2927d0ae4fe509a0ca3c9
Kelsey Tobin <etobinkelsey@gmail.com>
2018-12-12 07:33:57 -0700

    added tallyScore function and debugged error

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,68 +1,94 @@
-//make crystal values available to all functions
+//values available to all functions
 var crystal1 = 0;
 var cyrstal2 = 0;
 var crystal3 = 0;
 var crystal4 = 0;
 var totalScore = 0;
 var randomNumber = "";
+var wins = 0;
+var losses = 0;
 
-//function that makes other functions available after doucument is loaded-what needs to happen before the user can start playing
-$(document).ready(function() {
-  //create the random number var and random number forumla, setting max number that could be generated to 50
-  randomNumber = Math.floor(Math.random() * 100) + 1;
+//declare functions
 
-  //insert into random-number div
-  $("#number-text").text(randomNumber);
+  function getNumber() {
+    //create the random number var and random number forumla, setting max number that could be generated to 50
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+    //insert into random-number div
+    $("#number-text").text(randomNumber);
+  };
 
-  //1st crystal process-generate/assign/hide
-  crystal1 = Math.floor(Math.random() * 11) + 1;
-  $("#crystalbutton1").data("crystal1");
-  $("crystal1").hide("crystal1");
-  
-  console.log(crystal1);
+  function getCrystalValue() {
+    //1st crystal process-generate/assign/hide
+    crystal1 = Math.floor(Math.random() * 11) + 1;
+    $("#crystalbutton1").data("crystal1");
+    $("crystal1").hide("crystal1");
 
-  //2nd crystal procss-generate/assign/hide
-  crystal2 = Math.floor(Math.random() * 11) + 1;
-  $("#crystalbutton2").data("crystal2");
-  $("crystal2").hide("crystal2");
+    //2nd crystal procss-generate/assign/hide
+    crystal2 = Math.floor(Math.random() * 11) + 1;
+    $("#crystalbutton2").data("crystal2");
+    $("crystal2").hide("crystal2");
 
-  //3rd crystal process-generate/assign/hide
-  crystal3 = Math.floor(Math.random() * 11) + 1;
-  $("#crystalbutton3").data("crystal3");
-  $("#crystal3").hide("crystal3");
+    //3rd crystal process-generate/assign/hide
+    crystal3 = Math.floor(Math.random() * 11) + 1;
+    $("#crystalbutton3").data("crystal3");
+    $("#crystal3").hide("crystal3");
 
-  //4th crystal process-generate/assign/hide
-  crystal4 = Math.floor(Math.random() * 11) + 1;
-  $("#crystalbutton4").data("crystal4");
-  $("crystal4").hide("crystal4");
-});
+    //4th crystal process-generate/assign/hide
+    crystal4 = Math.floor(Math.random() * 11) + 1;
+    $("#crystalbutton4").data("crystal4");
+    $("crystal4").hide("crystal4");
+  };
 
 //game is ready to be played
 
-//creat crystal 1 click function
-$("#crystalbutton1").click("click", function() {
-  //add crystal1 value to total score var
-  totalScore += crystal1;
-  $("#score-text").text(totalScore);
-});
-
-//crystal 2 click function
-$("#crystalbutton2").click("click", function() {
-  totalScore += crystal2;
-  $("#score-text").text(totalScore);
-});
-
-
-//crystal 3 click function
-$("#crystalbutton3").click("click", function() {
-  totalScore += crystal3;
-  $("#score-text").text(totalScore);
-});
-
-//crystal 4 click function
-$("#crystalbutton4").click("click", function() {
-  totalScore += crystal4;
-  $("#score-text").text(totalScore);
-});
-
 //create win-lose function
+
+function tallyScore() {
+   if (totalScore === randomNumber) {
+      wins++;
+     $("#w-text").text(wins);
+    }
+    else if (totalScore > randomNumber) {
+      losses++;
+      $("#l-text").text(losses);
+    }
+};
+
+
+//execute
+$(document).ready(function() {
+  //create crystal 1 click function
+  $("#crystalbutton1").click("click", function() {
+    //add crystal1 value to total score var
+    totalScore += crystal1;
+    $("#score-text").text(totalScore);
+    tallyScore();
+  });
+  
+  //crystal 2 click function
+  $("#crystalbutton2").click("click", function() {
+    totalScore += crystal2;
+    $("#score-text").text(totalScore);
+    tallyScore();
+  });
+  
+  //crystal 3 click function
+  $("#crystalbutton3").click("click", function() {
+    totalScore += crystal3;
+    $("#score-text").text(totalScore);
+    tallyScore();
+  });
+
+  //crystal 4 click function
+  $("#crystalbutton4").click("click", function() {
+    totalScore += crystal4;
+    $("#score-text").text(totalScore);
+    tallyScore();
+  });
+
+  getNumber();
+  getCrystalValue();
+});
+
+
+
